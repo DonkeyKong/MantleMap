e71cd1441ecd0b5499859a088a4cb59fc013f9d4
DonkeyKong <admdonkeykong@gmail.com>
2022-04-08 02:08:39 -0400

    Improve gravity sim stability

--- include/Attributes.hpp
+++ include/Attributes.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <math.h>
+
 struct HSVColor;
 struct Color;
 
@@ -27,18 +29,74 @@ enum class FontStyle
 };
 
 #pragma pack(push, 1)
-struct Position
+struct Vec3
 {
   float x{0}; 
   float y{0}; 
   float z{0};
+  float mag2()
+  {
+    return pow(x,2.0f) +  pow(y,2.0f) + pow(z,2.0f);
+  }
+  float mag()
+  {
+    return sqrt(pow(x,2.0f) +  pow(y,2.0f) + pow(z,2.0f));
+  }
+  Vec3 operator-(const Vec3& o) const
+  {
+    return 
+    {
+      x-o.x,
+      y-o.y,
+      z-o.z
+    };
+  }
+  Vec3 operator+(const Vec3& o) const
+  {
+    return 
+    {
+      x+o.x,
+      y+o.y,
+      z+o.z
+    };
+  }
+  Vec3 operator+=(const Vec3& o)
+  {
+    return 
+    {
+      x+=o.x,
+      y+=o.y,
+      z+=o.z
+    };
+  }
+  Vec3 operator*(const float& s) const
+  {
+    return 
+    {
+      x*s,
+      y*s,
+      z*s
+    };
+  }
+  Vec3 operator/(const float& s) const
+  {
+    return 
+    {
+      x/s,
+      y/s,
+      z/s
+    };
+  }
 };
 
-struct Position2D
+struct Vec2
 {
   float x{0}; 
   float y{0};
 };
+
+typedef Vec3 Position;
+typedef Vec2 Position2D;
 
 struct TexCoord
 {
@@ -80,7 +138,7 @@ struct PhysicsPoint
   Color color{1,1,1,1};
   float size{1};
   float mass{1};
-  Position velocity{0,0,0};
+  Vec3 velocity{0,0,0};
 };
 
 #pragma pack(pop)
--- src/PhysicsScene.cpp
+++ src/PhysicsScene.cpp
@@ -19,7 +19,7 @@ void PhysicsScene::showOverride()
     {
         points[i].pos = Random(Position{0.0f, 0.0f, 0.0f}, Position{(float)config.width, (float)config.height, 0.0f});
         points[i].size = Random(0.5f, 3.0f);
-        points[i].mass = points[i].size * 5.0f; //Random(10.0f, 20.0f);
+        points[i].mass = pow(points[i].size, 2.0f) * 3.0f; //Random(10.0f, 20.0f);
         points[i].velocity = Random(Position{-0.05f, -0.05f, 0.0f}, Position{0.05f, 0.05f, 0.0f});
         points[i].color = Random(HSVColor(0.0f, 1.0f, 0.5f, 1.0f), HSVColor(360.0f, 1.0f, 0.6f, 1.0f));
     }
@@ -48,7 +48,7 @@ void PhysicsScene::initGLOverride()
     {
         // Load and compile the shaders into a glsl program
         program = loadGraphicsProgram("particlevert.glsl", "particlefrag.glsl");
-        program.SetCameraFromPixelTransform(config.width,config.height);
+        program.SetCameraFromPixelTransform(config.width,config.height); //,config.width/2.0f,config.height/2.0f,0.1f);
         vertexAttrib = glGetAttribLocation(program.GetId(), "aVertex");
         colorAttrib = glGetAttribLocation(program.GetId(), "aColor");
         pointSizeAttrib = glGetAttribLocation(program.GetId(), "aPointSize");
@@ -97,32 +97,48 @@ void PhysicsScene::drawOverride()
     }
 }
 
-static inline void gravForce(const Position& p1, const float& m1, const Position& p2, const float& m2, Position& a1, Position& a2)
+static inline bool gravForce(const PhysicsPoint& p1, const PhysicsPoint& p2, Vec3& a1, Vec3& a2)
 {
-    const float G = 0.001f;
-    // F = m1*m2 / dist^2
-    float d2 = pow(p1.x-p2.x,2.0f) +  pow(p1.y-p2.y,2.0f);
-    if (d2 < 1.0f)
+    const float G = 0.01f;
+    float d2 = (p1.pos-p2.pos).mag2();
+    float d = sqrt(d2);
+
+    if (d < (p1.size+p2.size / 3.0f))
     {
         a1.x = 0.0f;
         a1.y = 0.0f;
         a2.x = 0.0f;
         a2.y = 0.0f;
+        return true;
     }
-    else
-    {
-        float d = sqrt(d2);
-        float a1Mag = m2/d2*G;
-        float a2Mag = m1/d2*G;
-        a1.x = (p2.x - p1.x) / d * a1Mag;
-        a1.y = (p2.y - p1.y) / d * a1Mag;
-        a2.x = (p1.x - p2.x) / d * a2Mag;
-        a2.y = (p1.y - p2.y) / d * a2Mag;
-    }
+
+    // F = m1*m2 / dist^2
+    float F = p1.mass*p2.mass / d2 * G;
+    Vec3 norm1 = (p2.pos-p1.pos)/d;
+    Vec3 norm2 = (p1.pos-p2.pos)/d;
+    a1 = norm1 * F * (p2.mass) / (p1.mass+p2.mass);
+    a2 = norm2 * F * (p1.mass) / (p1.mass+p2.mass);
+    return false;
+}
+
+static inline void merge(PhysicsPoint& p1, PhysicsPoint& p2)
+{
+    float totalMass = p1.mass+p2.mass;
+
+    p1.pos = p1.pos * (p1.mass/totalMass) + p2.pos * (p2.mass/totalMass);
+    p1.size = sqrt(pow(p1.size,2.0f) + pow(p2.size,2.0f));
+    p1.velocity = p1.velocity * (p1.mass/totalMass) + p2.velocity * (p2.mass/totalMass);
+
+    p1.mass = p1.mass + p2.mass;
+    p2.size = 0;
+    p2.mass = 0;
+    p2.velocity = {0,0,0};
 }
 
 void PhysicsScene::updateOverride()
 {
+    const float dT = 0.1;
+
     updateCounter = (updateCounter + 1) % 30000;
 
     PhysicsPoint phantom
@@ -134,25 +150,35 @@ void PhysicsScene::updateOverride()
         {}
     };
 
-    Position a1, a2;
+    Vec3 a1, a2;
     for (int i=0; i < points.size(); i++)
     {
+        if (points[i].mass == 0)
+            continue;
+
         for (int j=i+1; j < points.size(); j++)
         {
-            gravForce(points[i].pos, points[i].mass, points[j].pos, points[j].mass, a1, a2);
-            points[i].velocity.x += a1.x;
-            points[i].velocity.y += a1.y;
-            points[j].velocity.x += a2.x;
-            points[j].velocity.y += a2.y;
+            if (points[j].mass == 0)
+                continue;
+
+            bool mergePoints = gravForce(points[i], points[j], a1, a2);
+
+            // if (mergePoints)
+            // {
+            //     merge(points[i], points[j]);
+            // }
+            // else
+            {
+                points[i].velocity += a1 * dT;
+                points[j].velocity += a2 * dT;
+            }
         }
 
         // Apply the phantom centering acceleration
-        gravForce(points[i].pos, points[i].mass, phantom.pos, phantom.mass, a1, a2);
-        points[i].velocity.x += a1.x;
-        points[i].velocity.y += a1.y;
+        gravForce(points[i], phantom, a1, a2);
+        points[i].velocity += a1 * dT;
 
         // Move the point for this time step
-        points[i].pos.x += points[i].velocity.x;
-        points[i].pos.y += points[i].velocity.y;
+        points[i].pos += points[i].velocity * dT;
     }
 }
